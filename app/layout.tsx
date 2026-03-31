c574f80c18e77907ced17fb1726b35e0c3db03d1
MateuszLisowski <mateuszlisowski@MacBook-Air-MateuszLisowski.local>
2026-04-01 00:09:49 +0200

    push

--- app/layout.tsx
+++ app/layout.tsx
@@ -115,7 +115,10 @@ export default function RootLayout({
             </svg>
           </a>
         </div>
-        <Newsletter />
+        <getresponse-form
+          form-id="dd268582-eedb-4b37-affb-517230ecf0d4"
+          e="0"
+        ></getresponse-form>
         <footer className="w-full mt-15 h-12 flex items-center justify-center border-t">
           Copyright © 2026 Mateusz Lisowski. Wszelkie prawa zastrzeżone.
         </footer>
--- app/newsletter.tsx
+++ app/newsletter.tsx
@@ -1,6 +1,17 @@
 "use client";
 
+import { useEffect } from "react";
+
 export default function Newsletter() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://an.gr-wcon.com/script/1255ad3d-46d3-4d82-8d63-34387f4a8b30/ga.js";
+    script.async = true;
+    script.crossOrigin = "use-credentials";
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <div>
       <getresponse-form
