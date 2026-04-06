f596b9fa56c4627dda9ee25c07f5b6253b986269
MateuszLisowski <mateuszlisowski@MacBook-Air-MateuszLisowski.local>
2026-04-06 23:24:29 +0200

    add changes to text

--- app/blog/posts.ts
+++ app/blog/posts.ts
@@ -14,4 +14,30 @@ export const posts = [
       <p>Minimalistycznie kończę nie pisząc mądrych słów i podsumowań.</p>
     `,
   },
+
+  {
+    slug: "zycie-po-terapii",
+    title: "Jak wygląda życie po terapii?",
+    excerpt:
+      "Nie chodzi o to, że wszystko staje się łatwe. Raczej o to, że zaczynasz mieć wybór.",
+    date: "2026-04-06",
+    img: "/blog/zycie-po-terapii.jpeg",
+    content: `
+      <img src="/blog/zycie-po-terapii.jpeg" alt="zdjecie" class="mb-5 mt-10 rounded-xl" />
+
+      <p class="my-5">Zastanawiałeś się może, czym różni się życie przed terapią od tego po terapii? Wbrew pozorom rzadko można znaleźć na to pytanie odpowiedź, która jest szczera i w miarę rzetelna. Zależy mi, aby przedstawić sprawę uczciwie. Jak idziesz na wizytę do dentysty, mniej więcej wiesz, czego się spodziewać (chyba że jest to Meksyk i budzisz się bez nerki). W psychoterapii efekty pracy są trudno mierzalne, nieliniowe i w pewnym sensie nieuchwytne, tak jak inne parametry jakościowe zmiany. Nie jest tak, że nie ma możliwości zrobienia badań naukowych, aby sprawdzić efekty. Są badania, lecz często na małej próbie, często też mało losowej (np. studenci psychologii).</p>
+
+      <p class="my-5">Ale do rzeczy.</p>
+
+      <p class="my-5">Psychoterapia nie sprawia, że stajesz się kimś innym, nie czujesz, że jesteś niezniszczalny. Nie sprawia, że nagle zaczniesz mówić innym językiem czy żargonem terapeutycznym. Nie sprawi, że odejdziesz z korporacji czy wyjedziesz do Malezji uprawiać ryż na plantacji, żyjąc w ascetyczny sposób.</p>
+
+      <p class="my-5">Jeśli wszystko będzie iść w dobrym kierunku - czyli mając wzloty i upadki, a w momencie kryzysu w terapii nie zrezygnujesz nagle, kiedy najbardziej potrzebujesz wsparcia - największą zmianą będzie to, że zaczniesz mieć wybór. W momencie, gdy nie masz wyboru, żyjesz automatycznie, nie wiesz, co czujesz, dlaczego i co możesz zrobić inaczej, żeby coś zmienić. Dzięki temu będziesz bardziej sobą. Podejmować decyzje i zachowania zgodniejsze z tym, kim jesteś, robić rzeczy, które naprawdę chcesz robić i nie blokować się przed wyrażaniem siebie w sposób, jaki potrzebujesz. Jeśli nie będziesz chciał nic mówić, to nie będziesz - tak samo jak, gdy zapragniesz porozmawiać, będziesz potrafił po to sięgnąć i zaspokoić swoją potrzebę.</p>
+
+      <p class="my-5">Największą zmianą będzie to, że zaczniesz mieć wybór.</p>
+
+      <p class="my-5">Nie będzie idealnie. Będziesz cierpieć, rozpaczać, płakać z bólu, przeżywać, potykać się o te same przeszkody, które ciągle będą Cię prześladować. Najlepszym cytatem, z którym się zgadzam, jest to, że terapia polega na odjęciu niepotrzebnego cierpienia. Przykładowo, jeśli jakaś sytuacja jest smutna, będziesz potrafił się smucić, ale jeśli wszystko jest smutne, terapia może pomóc wyrwać się z pętli nadmiernego smutku przeżywanego cały czas. Zrobi to miejsce na inne emocje, przeżycia i sprawy. Nie jest to życie idealne, ale ludzkie, niedoskonałe i prawdziwe.</p>
+
+      <p class="my-5">Nie zachęcam nikogo do terapii, bo ten proces może boleć i mieć skutki uboczne. Ale jeśli czujesz, że czegoś jest za dużo i nie wiesz, jak z tego wyjść, warto spróbować dać sobie szansę na zmianę. Jak nie wyjdzie, to chociaż próbowałeś.</p>
+    `,
+  },
 ];
